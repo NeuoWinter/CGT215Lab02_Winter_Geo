8f8673a69e0e815dbc9609c90b5d086a9db8d010
Geo Winter <winter66@purdue.edu>
2023-08-31 10:04:18 -0400

    Lab02

--- CGT215Lab02_Winter_Geo/CGT215Lab02_Winter_Geo.cpp
+++ CGT215Lab02_Winter_Geo/CGT215Lab02_Winter_Geo.cpp
@@ -1,20 +1,35 @@
 // CGT215Lab02_Winter_Geo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float A; // declare vars
+    float B;
+    float X;
+
+    // welcome message
+    cout << "================================================================" << endl;
+    cout << "||                           Lab 02                           ||" << endl;
+    cout << "================================================================" << endl << endl;
+    cout << "Welcome, my name is Geo, and this is how to solve the equation: " << endl;
+    cout << "AX + B = 0 and find X!" << endl << endl;
+
+    // prompt for A and B vars
+    cout << "Please enter a value for A: ";
+    cin >> A;
+    cout << "Please enter a value for B: ";
+    cin >> B;
+
+    cout << endl << "Solving " << A << "x + " << B << " = 0..." << endl << endl;
+
+    // output
+    cout << "The answer is: " << endl;
+    X = (0 - B) / A;
+    cout << "X = " << X << endl << endl;
+    cout << "================================================================" << endl;
+    cout << "||                           Lab 02                           ||" << endl;
+    cout << "================================================================" << endl << endl;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
